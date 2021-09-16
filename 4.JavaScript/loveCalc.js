8d041a75fe1b8d52b0c2c037a4218b4d7886fd5b
Praveen Kumar S <55323914+Stonekeeper@users.noreply.github.com>
2021-09-16 03:17:19 +0000

    Create loveCalc.js

---
+++ 4.JavaScript/loveCalc.js
@@ -0,0 +1,10 @@
+function loveCalc(){
+prompt("Name of 1st Person");
+prompt("Name of 2nd Person");
+
+var love=Math.random();
+    love=love*100+1;
+    love=Math.floor(love);
+
+alert("Love B/W both people is "+love+"%");
+}
