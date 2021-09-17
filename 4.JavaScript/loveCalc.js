d7b4eac8aadb2cad4ab3af0fc9941bd92ea85901
Praveen Kumar S <55323914+Stonekeeper@users.noreply.github.com>
2021-09-17 02:29:30 +0000

    Update loveCalc.js

--- 4.JavaScript/loveCalc.js
+++ 4.JavaScript/loveCalc.js
@@ -1,4 +1,3 @@
-function loveCalc(){
 prompt("Name of 1st Person");
 prompt("Name of 2nd Person");
 
@@ -6,5 +5,10 @@ var love=Math.random();
     love=love*100+1;
     love=Math.floor(love);
 
-alert("Love B/W both people is "+love+"%");
-}
+    if(love>65){
+       alert("Love B/W both people is "+love+"% Blah Blah Blah");
+       } else if(love>30 && love<=65) { 
+       alert("Love B/W both people is "+love+"%. Noice...");
+       } else if(love<=30){
+         alert("Love B/W both people is "+love+"%.");
+       }
