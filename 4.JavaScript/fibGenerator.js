20ce7529454e1afe58bdfacca5fd819d09e918ad
Praveen Kumar S <55323914+Stonekeeper@users.noreply.github.com>
2021-09-18 02:56:38 +0000

    Create fibGenerator.js

---
+++ 4.JavaScript/fibGenerator.js
@@ -0,0 +1,24 @@
+function fibGenerator(n)
+{
+    var output=[];
+
+    if(n===1)
+        output=[0];
+    else
+    if(n===2)
+        output=[0,1];
+    else 
+    {
+        output=[0,1];
+
+        for(var i=2;i<n;i++)
+        {
+            output.push(output[output.length-2]+output[output.length-1]);
+        }
+    }
+    return output;
+}
+
+var n=prompt("Enter the number");
+output=fibGenerator(n);
+console.log(output);
